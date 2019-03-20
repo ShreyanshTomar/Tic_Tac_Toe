6f9b4920232447ef0300f43ee74ddf6665f4f916
Shreyansh Tomar <shreyanshtomar99@gmail.com>
2019-03-20 17:17:24 +0530

    Updated Tic_Tac_Toe
    Now works for python 3!

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -13,51 +13,51 @@ class color:
 board = [1,2,3,4,5,6,7,8,9]
 count = 0
 def Board():
-    print '+-----------+'
-    print '| {} | {} | {} |'.format(board[0],board[1],board[2])
-    print '+-----------+'
-    print '| {} | {} | {} |'.format(board[3],board[4],board[5])
-    print '+-----------+'
-    print '| {} | {} | {} |'.format(board[6],board[7],board[8])
-    print '+-----------+'
+    print ("+-----------+")
+    print ("| {} | {} | {} |".format(board[0],board[1],board[2]))
+    print ("+-----------+")
+    print ("| {} | {} | {} |".format(board[3],board[4],board[5]))
+    print ("+-----------+")
+    print ("| {} | {} | {} |".format(board[6],board[7],board[8]))
+    print ("+-----------+")
    
 def userdata():
-    Position = input("Enter the position you want to mark your symbol on:")
-    Marker = raw_input("Enter your Symbol: 'X' or 'O':")
+    Position = int(input("Enter the position you want to mark your symbol on:"))
+    Marker = input("Enter your Symbol: 'X' or 'O':")
     if(Marker == 'X' or Marker == 'O'):
         board[Position-1] = Marker
         Board()
     else:
-        print color.BOLD + "Please Re-enter the valid inputs" + color.END
+        print ("Please Re-enter the valid inputs")
         userdata()
     
     
 def start_tic_tac_toe():
     global count
     global board
-    print color.RED + "Welcome to Tic_Tac_Toe!" + color.END
+    print ("Welcome to Tic_Tac_Toe!")
     Board()
-    for turn in xrange(1,10):
+    for turn in range(1,10):
         if turn%2 == 1:
-            print "Player 1 it's your turn"
-            print "Your symbol is 'X'"
+            print ("Player 1 it's your turn")
+            print ("Your symbol is 'X'")
             userdata()
             if(board[0]==board[1]==board[2] or board[0]==board[3]==board[6] or board[2]==board[5]==board[8] or board[6]==board[7]==board[8] or board[3]==board[4]==board[5] or board[0]==board[4]==board[8] or board[2]==board[4]==board[6]):
-                print 'Player 1 wins  _へ__(‾◡◝ )>'
-                print '＼（＾○＾）人（＾○＾）／'
-                print 'Player 2 loses ⊙﹏⊙'
+                print ("Player 1 wins")
+                
+                print ("Player 2 loses")
                 break
             else:
                 count += 1
                 pass
         elif turn%2 == 0:
-            print "Player 2 it's your turn"
-            print "Your symbol is 'O'"
+            print ("Player 2 it's your turn")
+            print ("Your symbol is 'O'")
             userdata()
             if(board[0]==board[1]==board[2] or board[0]==board[3]==board[6] or board[2]==board[5]==board[8] or board[6]==board[7]==board[8] or board[3]==board[4]==board[5] or board[0]==board[4]==board[8] or board[2]==board[4]==board[6]):
-                print 'Player 2 wins  _へ__(‾◡◝ )>'
-                print '＼（＾○＾）人（＾○＾）／'
-                print 'Player 1 loses ⊙﹏⊙'
+                print ("Player 2 wins")
+                print ("＼（＾○＾）人（＾○＾）／")
+                print ('Player 1 loses ⊙﹏⊙')
                 break
             else:
                 count += 1
@@ -67,14 +67,14 @@ def start_tic_tac_toe():
     if(count == 9):
         board = [1,2,3,4,5,6,7,8,9]
         count = 0
-        print "Ohh!"
-        print "It's a tie!  o(〃＾▽＾〃)o"
-        print "Want to play again?\nY for Yes or N for No "
-        tie = raw_input()
+        print ("Ohh!")
+        print ("It's a tie!  o(〃＾▽＾〃)o")
+        print ("Want to play again?\nY for Yes or N for No ")
+        tie = input()
         if tie == 'Y':
             start_tic_tac_toe()
         else:
-            print "Thanks for Playing! :)"
+            print ("Thanks for Playing! :)")
             pass
     else:
         board = [1,2,3,4,5,6,7,8,9]
